920ff4c180ad371d149c714d6023354f15dfb621
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-04-25 13:40:07 +0300

    added midis copy script

--- scripts/split_into_new_groups.py
+++ scripts/split_into_new_groups.py
@@ -32,7 +32,6 @@ def create_new_noteEM_directory(old_noteEM_path: str, new_noteEM_path: str, num_
     
     for root, dirs, files in os.walk(old_noteEM_path):
         for file in files:
-            exit(1)
             song_id, shift = file.split('#')
             shift = shift.split('.')[0]
             group = song_to_group_dict[song_id]
@@ -42,7 +41,32 @@ def create_new_noteEM_directory(old_noteEM_path: str, new_noteEM_path: str, num_
     print("created successfuly new noteEM dir")
 
 
+def create_new_midi_dirs_per_group(old_midis_dir_path: str, new_midis_dir_path: str, groups_json_file_path: str):
+    with open(groups_json_file_path, 'r') as fp:
+        song_to_group_dict = json.load(fp)
+    groups = list(set(song_to_group_dict.values()))
+
+    for g in groups:
+        os.makedirs(os.path.join(new_midis_dir_path, f"group{g}"), exist_ok=True)
+
+    for root, dirs, files in os.walk(old_midis_dir_path):
+        for file in files:
+            if not file.endswith('.mid'):
+                continue
+            song_id = file.split('#')[0]
+            group = song_to_group_dict[song_id]
+            destination = os.path.join(new_midis_dir_path, f"group{group}", file)
+            source = os.path.join(root, file)
+            shutil.copy(source, destination)
+    print("created new midis dir")
+      
+
+
 if __name__ == "__main__":
     # song_list = list_all_songs_in_dir("/vol/scratch/jonathany/datasets/full_musicnet/noteEM_audio")
     # split_song_list_to_groups(song_list, 20)
-    create_new_noteEM_directory("/vol/scratch/jonathany/datasets/full_musicnet/noteEM_audio", "/vol/scratch/jonathany/datasets/musicnet_groups_of_20/noteEM_audio", 20)
+    # create_new_noteEM_directory("/vol/scratch/jonathany/datasets/full_musicnet/noteEM_audio", "/vol/scratch/jonathany/datasets/musicnet_groups_of_20/noteEM_audio", 20)
+    old_path = "/vol/scratch/jonathany/datasets/full_musicnet"
+    new_path = "/vol/scratch/jonathany/datasets/musicnet_groups_of_20/midis"
+    groups_json_path = "/vol/scratch/jonathany/datasets/musicnet_groups_of_20/noteEM_audio/song_to_group_dict.json"
+    create_new_midi_dirs_per_group(old_path, new_path, groups_json_path)
