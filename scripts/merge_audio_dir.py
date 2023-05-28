9fdc3d2074824650b8287386a9595aafe98ded85
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-05-29 00:33:49 +0300

    fixed script bugs

--- scripts/merge_audio_dir.py
+++ scripts/merge_audio_dir.py
@@ -15,13 +15,16 @@ def merge_audio_dir(src_dir, res_dir, group_name):
             shft = int(name.split('#')[-1].split('.flac')[0])
             src_path = os.path.join(root, name)
             dst_path = os.path.join(res_dir, f"{group_name}#{shft}", name)
-            shutil.copy(src=src_path, dst=dst_path)
+            # shutil.copy(src=src_path, dst=dst_path)
+            # createing a sym link is the more efficient method
+            os.symlink(src=src_path, dst=dst_path)
             counter += 1
         
     print(f"Finished merging, overall found {counter} files.")
 
 
 def add_virtual_paths(src_dir_path, merged_dir_path):
+    os.makedirs(merged_dir_path, exist_ok=True)
     for f in os.listdir(src_dir_path):
         curr_dir_path = os.path.join(src_dir_path, f)
         if os.path.isdir(curr_dir_path) and f.count('#') == 1:
@@ -33,10 +36,10 @@ def add_virtual_paths(src_dir_path, merged_dir_path):
 
 if __name__ == "__main__":
     # audio_path = '/vol/scratch/jonathany/datasets/Museopen16'
-    # mid_path = '/vol/scratch/jonathany/datasets/AllTranscriptions'
-    # res_path = '/vol/scratch/jonathany/datasets/Museopen_merged'
+    # # mid_path = '/vol/scratch/jonathany/datasets/AllTranscriptions'
+    # res_path = '/vol/scratch/jonathany/datasets/full_museopen/noteEM_audio'
     # merge_audio_dir(audio_path, res_path, 'full_museopen')
-    src_dir = '/vol/scratch/jonathany/datasets/full_museopen/noteEM_audio'
+    src_dir = '/vol/scratch/jonathany/datasets/full_musicnet_with_piano_random_shift/noteEM_audio'
     dst = '/vol/scratch/jonathany/datasets/museopen_and_musicnet/noteEM_audio'
     add_virtual_paths(src_dir_path=src_dir, merged_dir_path=dst)
             
