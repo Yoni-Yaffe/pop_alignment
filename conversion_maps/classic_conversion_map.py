bfa38c595c0cc7b508e4bfdfefe01d7246c437b7
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-06-23 20:03:50 +0300

    updated congfig file to musicnet train and also added ensemble to classic conversion map

--- conversion_maps/classic_conversion_map.py
+++ conversion_maps/classic_conversion_map.py
@@ -8,6 +8,8 @@ GUITAR = list(range(24, 40)) + [6, 46, 45]
 
 ### STRINGS
 STRINGS = [40, 41, 42, 43, 48, 49, 105]
+ENSAMBLE = [48, 49, 50, 51, 52, 53, 54, 55]
+STRINGS_CLASS = STRINGS #+ ENSAMBLE
 
 ### WINDS
 TRUMPET = [60, 56, 57, 59]
@@ -17,8 +19,10 @@ OBOE = [68, 69]
 PIPE = [73, 72, 74, 75, 76, 77, 78, 79]
 WIND = TRUMPET + [58, 60] + BRASS_SECTION + SAX + OBOE + PIPE + [70, 71, 72, 73] + [52]
 
+# Percussion
+TIMPANI_AND_VIBRAPHONE = [48, 11]
 
-ALL_CLASSES = [PIANO + ORGAN, GUITAR, STRINGS,  WIND, [47, 11]]
+ALL_CLASSES = [PIANO + ORGAN, GUITAR, STRINGS_CLASS,  WIND, TIMPANI_AND_VIBRAPHONE]
 inst_class_dict = {c[0]: c for c in ALL_CLASSES}
 
 def reverse_dict(d: dict) -> dict:
--- scripts/merge_audio_dir.py
+++ scripts/merge_audio_dir.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     # res_path = '/vol/scratch/jonathany/datasets/full_museopen/noteEM_audio'
     # merge_audio_dir(audio_path, res_path, 'full_museopen')
     src_dir = '/vol/scratch/jonathany/datasets/Museopen16'
-    dst = '/vol/scratch/jonathany/datasets/Bach Trio Sonatas/noteEM_audio'
+    dst = '/vol/scratch/jonathany/datasets/all_museopen_groups_group/noteEM_audio'
     add_virtual_paths(src_dir_path=src_dir, merged_dir_path=dst)
             
     
