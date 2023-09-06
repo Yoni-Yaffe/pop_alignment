0014d69d2659e77837e2b127a9793a6182937fa3
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-09-06 10:59:33 +0300

    generate labels added pitch ckpt and mask options

--- generate_labels.py
+++ generate_labels.py
@@ -9,7 +9,7 @@ import os
 import torch.nn.functional as F
 from einops import rearrange
 from onsets_and_frames.transcriber import ModulatedOnsetsAndFrames, ModulatedOnsetsAndFramesGroup
-
+from onsets_and_frames.utils import max_inst
 
 def set_diff(model, diff=True):
     for layer in model.children():
@@ -30,7 +30,7 @@ def load_audio(flac):
     return audio
 
 
-def inference_single_flac(transcriber, flac_path, inst_mapping, out_dir, modulated_transcriber=False):
+def inference_single_flac(transcriber, flac_path, inst_mapping, out_dir, modulated_transcriber=False, use_max_inst=True, pitch_transcriber=None, mask=None):
     if isinstance(transcriber, DataParallel):
         transcriber_module = transcriber.module
     else:
@@ -53,6 +53,11 @@ def inference_single_flac(transcriber, flac_path, inst_mapping, out_dir, modulat
         offset_preds = []
         frame_preds = []
         vel_preds = []
+        
+        pitch_onsets_preds = []
+        pitch_offset_preds = []
+        pitch_frame_preds = []
+        pitch_vel_preds = []
         for i_s in range(n_segments):
             curr = audio_inp[i_s * seg_len: (i_s + 1) * seg_len].unsqueeze(0).cuda()
             curr_mel = melspectrogram(curr.reshape(-1, curr.shape[-1])[:, :-1]).transpose(-1, -2)
@@ -60,26 +65,38 @@ def inference_single_flac(transcriber, flac_path, inst_mapping, out_dir, modulat
                 batch_mel = curr_mel.repeat(len(inst_mapping) + 1, 1, 1)
                 instruments = F.one_hot(torch.arange(len(inst_mapping) + 1)).to(torch.float32).to('cuda')
                 if modulated_inst:
-                    curr_onset_pred, curr_offset_pred, _, curr_frame_pred, curr_velocity_pred = transcriber(batch_mel, instruments)
+                    curr_onset_pred, curr_offset_pred, _, curr_frame_pred, *_ = transcriber(batch_mel, instruments)
                 else:
                     groups = torch.zeros((len(inst_mapping) + 1, n_groups)).to(torch.float32).to('cuda')
-                    curr_onset_pred, curr_offset_pred, _, curr_frame_pred, curr_velocity_pred = transcriber(batch_mel, instruments, groups)
+                    curr_onset_pred, curr_offset_pred, _, curr_frame_pred, *_ = transcriber(batch_mel, instruments, groups)
                     
                 curr_onset_pred = rearrange(curr_onset_pred, 'i t n -> 1 t (i n)')
                 curr_frame_pred = rearrange(curr_frame_pred.max(axis=0)[0], 't n -> 1 t n')
-                curr_offset_pred, curr_velocity_pred = curr_offset_pred[:1], curr_velocity_pred[:1]
+                curr_offset_pred= curr_offset_pred[:1]
                     
                 
             else:
-                curr_onset_pred, curr_offset_pred, _, curr_frame_pred, curr_velocity_pred = transcriber(curr_mel)
+                curr_onset_pred, curr_offset_pred, _, curr_frame_pred, *_ = transcriber(curr_mel)
+            if pitch_transcriber is not None:
+                pitch_curr_onset_pred, pitch_curr_offset_pred, _, pitch_curr_frame_pred, *_ = pitch_transcriber(curr_mel)
+                pitch_onsets_preds.append(pitch_curr_onset_pred)
+                pitch_offset_preds.append(pitch_curr_offset_pred)
+                pitch_frame_preds.append(pitch_curr_frame_pred)
+                # pitch_vel_preds.append(pitch_curr_velocity_pred)
             onsets_preds.append(curr_onset_pred)
             offset_preds.append(curr_offset_pred)
             frame_preds.append(curr_frame_pred)
-            vel_preds.append(curr_velocity_pred)
+            # vel_preds.append(curr_velocity_pred)
         onset_pred = torch.cat(onsets_preds, dim=1)
         offset_pred = torch.cat(offset_preds, dim=1)
         frame_pred = torch.cat(frame_preds, dim=1)
-        velocity_pred = torch.cat(vel_preds, dim=1)
+        # velocity_pred = torch.cat(vel_preds, dim=1)
+        if pitch_transcriber is not None:
+            pitch_onset_pred = torch.cat(pitch_onsets_preds, dim=1)
+            pitch_offset_pred = torch.cat(pitch_offset_preds, dim=1)
+            pitch_frame_pred = torch.cat(pitch_frame_preds, dim=1)
+            # pitch_velocity_pred = torch.cat(pitch_vel_preds, dim=1)
+            
     else:
         print("didn't have to split")
         audio_inp = audio_inp.unsqueeze(0).cuda()
@@ -88,40 +105,80 @@ def inference_single_flac(transcriber, flac_path, inst_mapping, out_dir, modulat
             batch_mel = mel.repeat(len(inst_mapping) + 1, 1, 1)
             instruments = F.one_hot(torch.arange(len(inst_mapping) + 1)).to(torch.float32).to('cuda')
             if modulated_inst:
-                onset_pred, offset_pred, _, frame_pred, velocity_pred = transcriber(batch_mel, instruments)
+                onset_pred, offset_pred, _, frame_pred, *_ = transcriber(batch_mel, instruments)
             else:
                 groups = torch.zeros((len(inst_mapping) + 1, n_groups)).to(torch.float32).to('cuda')
-                onset_pred, offset_pred, _, frame_pred, velocity_pred = transcriber(batch_mel, instruments, groups)
-            offset_pred, velocity_pred = offset_pred[:1], velocity_pred[:1]
+                onset_pred, offset_pred, _, frame_pred, *_ = transcriber(batch_mel, instruments, groups)
+            offset_pred = offset_pred[:1]
             onset_pred = rearrange(onset_pred, 'i t n -> 1 t (i n)')
             frame_pred = rearrange(frame_pred.max(axis=0)[0], 't n -> 1 t n')
         else:
-            onset_pred, offset_pred, _, frame_pred, velocity_pred = transcriber(mel)
+            onset_pred, offset_pred, _, frame_pred, *_ = transcriber(mel)
+            if pitch_transcriber is not None:
+                pitch_onset_pred, pitch_offset_pred, _, pitch_frame_pred, *_ = pitch_transcriber(mel)
+                
+                
+                
 
     onset_pred = onset_pred.detach().squeeze().cpu()
     frame_pred = frame_pred.detach().squeeze().cpu()
-
-    peaks = get_peaks(onset_pred, 3)  # we only want local peaks, in a 7-frame neighborhood, 3 to each side.
-    onset_pred[~peaks] = 0
+    
+    # peaks = get_peaks(onset_pred, 3)  # we only want local peaks, in a 7-frame neighborhood, 3 to each side.
+    # onset_pred[~peaks] = 0
 
     onset_pred_np = onset_pred.numpy()
     # onset_pred_np[:, :-N_KEYS * 5] = 0
     frame_pred_np = frame_pred.numpy()
+    print("onset_pred_np shape", onset_pred_np.shape)
+    print("frame_pred_np shape", frame_pred_np.shape)
+    print("$"*100)
+    print("inst mapping", inst_mapping)
+    print("$"*100)
+    if mask is not None:
+        mask_with_pitch = mask + [1]
+        mask_list = [np.full((onset_pred_np.shape[0], N_KEYS), i) for i in mask_with_pitch]
+        mask_array = np.hstack(mask_list)
+        print("mask shape", mask_array.shape)
+        print("mask array:", mask_array)
+        assert onset_pred_np.shape == mask_array.shape
+        onset_pred_np = mask_array * onset_pred_np
+
+    if pitch_transcriber is not None:
+        pitch_onset_pred = pitch_onset_pred.detach().squeeze().cpu()
+        pitch_frame_pred = pitch_frame_pred.detach().squeeze().cpu()
+        pitch_onset_pred_np = pitch_onset_pred.numpy()
+        print("pitch_onset_pred_np shape", pitch_onset_pred_np.shape)
+        # print("pitch_frame_pred_np shape", pitch_frame_pred_np.shape)
+        onset_pred_np[:, -88:] = np.maximum(pitch_onset_pred_np[:, -88:], onset_pred_np[:, -88:])
+        # onset_pred_np = np.maximum(pitch_onset_pred_np, onset_pred_np)
+        frame_pred_np = pitch_frame_pred.numpy()
 
     # save_path = 'Champions_League.mid'
     save_path = os.path.join(out_dir, os.path.basename(flac_path).replace('.flac', '.mid'))
-
+    
     inst_only = len(inst_mapping) * N_KEYS
+    print("inst only")
+    print("onset pred shape", onset_pred_np.shape)
+    if use_max_inst and len(inst_mapping) > 1:
+        print("used max inst !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        onset_pred_np = np.maximum(onset_pred_np, max_inst(onset_pred_np))
+        
     frames2midi(save_path,
                 onset_pred_np[:, : inst_only], frame_pred_np[:, : inst_only],
                 64. * onset_pred_np[:, : inst_only],
                 inst_mapping=inst_mapping)
+    # frames2midi(save_path,
+    #         onset_pred_np[:, -88:], frame_pred_np[:, -88:],
+    #         64. * onset_pred_np[:, : -88],
+    #         inst_mapping=inst_mapping)
     print(f"saved midi to {save_path}")
     return save_path
 
-def generate_labels(transcriber_ckpt, flac_dir, config):
+def generate_labels(transcriber_ckpt, flac_dir, config, pitch_ckpt=None, mask=None):
     # inst_mapping = [0, 68, 70, 71, 40, 73, 41, 42, 43, 45, 6, 60]
     # inst_mapping = [0, 68, 70, 71, 40, 73, 41, 42, 45, 6, 60]
+    pitch_transcriber = None
+    pitch_parallel_transcriber = None
     inst_mapping = config['inst_mapping']
 
     num_instruments = len(inst_mapping)
@@ -132,11 +189,26 @@ def generate_labels(transcriber_ckpt, flac_dir, config):
     set_diff(transcriber.frame_stack, False)
     set_diff(transcriber.offset_stack, False)
     set_diff(transcriber.combined_stack, False)
-    set_diff(transcriber.velocity_stack, False)
+    if hasattr(transcriber, 'velocity_stack'):
+        set_diff(transcriber.velocity_stack, False)
+    print("transcriber: ", transcriber)
 
     parallel_transcriber = DataParallel(transcriber)
 
     transcriber.zero_grad()
+    if pitch_ckpt:
+        pitch_transcriber = torch.load(transcriber_ckpt).to(device)
+        set_diff(pitch_transcriber.frame_stack, False)
+        set_diff(pitch_transcriber.offset_stack, False)
+        set_diff(pitch_transcriber.combined_stack, False)
+        if hasattr(pitch_transcriber, 'velocity_stack'):
+            set_diff(pitch_transcriber.velocity_stack, False)
+    
+
+        pitch_parallel_transcriber = DataParallel(pitch_transcriber)
+
+        pitch_transcriber.zero_grad()
+    
     print("cuda mem info:")
     print(torch.cuda.mem_get_info())
     torch.cuda.empty_cache()
@@ -151,7 +223,9 @@ def generate_labels(transcriber_ckpt, flac_dir, config):
                                   flac_path=flac_path,
                                   inst_mapping=inst_mapping, 
                                   out_dir=results_dir,
-                                  modulated_transcriber=config['modulated_transcriber'])
+                                  modulated_transcriber=config['modulated_transcriber'],
+                                  pitch_transcriber=pitch_parallel_transcriber,
+                                  mask=mask)
 
 
 def generate_labels_wrapper(yaml_config: dict):
@@ -159,10 +233,18 @@ def generate_labels_wrapper(yaml_config: dict):
     config['out_dir'] = yaml_config['logdir']
     ckpt = 'ckpts/transcriber_iteration_60001.pt'
     ckpt = config['ckpt']
+    if 'pitch_ckpt' in config:
+        pitch_ckpt = config['pitch_ckpt']
+    else:
+        pitch_ckpt = None
+    if 'mask' in config:
+        mask = config['mask']
+    else:
+        mask = None
     flac_dir = config['audio_files_dir']
     # flac_path = 'Champions_League#0.flac'
     flac_path = 'Westworld#0.flac'
-    generate_labels(ckpt, flac_dir, config)
+    generate_labels(ckpt, flac_dir, config, pitch_ckpt=pitch_ckpt, mask=mask)
 
 
 if __name__ == '__main__':
