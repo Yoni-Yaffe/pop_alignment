342d93e9df9254930fb5d0e8a1c251160e98a209
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-09-23 11:25:26 +0300

    changed constant conversion map to be suitable for pop instruments

--- conversion_maps/constant_conversion_map.py
+++ conversion_maps/constant_conversion_map.py
@@ -1,3 +1,3 @@
 from conversion_maps import classic_conversion_map
-
-conversion_map = {i: 0 for i in classic_conversion_map.conversion_map}
+from conversion_maps import pop_conversion_map
+conversion_map = {i: 0 for i in pop_conversion_map.conversion_map}
