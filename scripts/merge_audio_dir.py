de3c7f6e7180ee0ce500fccaeff585069f8af610
jYoni-Yaffe <jonathany@mail.tau.ac.il>
2023-09-30 19:56:36 +0300

    added more scripts for data handling

--- scripts/merge_audio_dir.py
+++ scripts/merge_audio_dir.py
@@ -22,6 +22,13 @@ def merge_audio_dir(src_dir, res_dir, group_name):
         
     print(f"Finished merging, overall found {counter} files.")
 
+def merge_midi(src, dst):
+    os.makedirs(dst, exist_ok=True)
+    for root, dirs, files in tqdm(os.walk(src, topdown=False)):
+        for name in files:
+            src_path = os.path.join(root, name)
+            dst_path = os.path.join(dst, name)
+            os.symlink(src=src_path, dst=dst_path)
 
 def add_virtual_paths(src_dir_path, merged_dir_path):
     os.makedirs(merged_dir_path, exist_ok=True)
@@ -32,15 +39,44 @@ def add_virtual_paths(src_dir_path, merged_dir_path):
             dst = os.path.join(merged_dir_path, f)
             os.symlink(src=src, dst=dst)
     
-
+def copy_all_files_virtual(src_dir_path, dst_dir_path):
+    os.makedirs(dst_dir_path, exist_ok=True)
+    for f in os.listdir(src_dir_path):
+        curr_dir_path = os.path.join(src_dir_path, f)
+        src = curr_dir_path
+        dst = os.path.join(dst_dir_path, f)
+        os.symlink(src=src, dst=dst)
+    
+def merge_museopen_and_musicnet(musicnet_dir, museopen_dir):
+    pass
 
 if __name__ == "__main__":
     # audio_path = '/vol/scratch/jonathany/datasets/Museopen16'
     # # mid_path = '/vol/scratch/jonathany/datasets/AllTranscriptions'
     # res_path = '/vol/scratch/jonathany/datasets/full_museopen/noteEM_audio'
     # merge_audio_dir(audio_path, res_path, 'full_museopen')
-    src_dir = '/vol/scratch/jonathany/datasets/Museopen16'
-    dst = '/vol/scratch/jonathany/datasets/all_museopen_groups_group/noteEM_audio'
-    add_virtual_paths(src_dir_path=src_dir, merged_dir_path=dst)
-            
     
+    audio_path = '/vol/scratch/jonathany/datasets/FULL_POP_AUDIO_SHIFT'
+    # # mid_path = '/vol/scratch/jonathany/datasets/AllTranscriptions'
+    res_path = '/vol/scratch/jonathany/datasets/full_pop_merged/noteEM_audio'
+    merge_audio_dir(audio_path, res_path, 'full_pop_merged')
+    
+    # midi_path = '/vol/scratch/jonathany/datasets/FULL_POP_MIDI'
+    # # # mid_path = '/vol/scratch/jonathany/datasets/AllTranscriptions'
+    # res_path = '/vol/scratch/jonathany/datasets/full_pop_merged_midi/full_pop_merged'
+    # merge_midi(midi_path, res_path)
+    
+    
+    # src_dir = '/vol/scratch/jonathany/datasets/full_museopen/noteEM_audio'
+    # dst = '/vol/scratch/jonathany/datasets/musicnet_and_museopen/noteEM_audio'
+    # add_virtual_paths(src_dir_path=src_dir, merged_dir_path=dst)
+    
+    # src_dir = '/vol/scratch/jonathany/datasets/full_musicnet_with_piano_random_shift/noteEM_audio'
+    # dst = '/vol/scratch/jonathany/datasets/musicnet_and_museopen/noteEM_audio'
+    # add_virtual_paths(src_dir_path=src_dir, merged_dir_path=dst)
+    
+    # merge_audio_dir(src_dir=src_dir, res_dir='/vol/scratch/jonathany/datasets/museopen_merged', group_name='museopen')
+            
+    # src_dir = '/vol/scratch/jonathany/datasets/full_musicnet_with_piano_random_shift/NoteEm_labels'
+    # dst_path = '/vol/scratch/jonathany/datasets/musicnet_and_museopen/NoteEm_labels'
+    # copy_all_files_virtual(src_dir, dst_path)
--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -1,5 +1,5 @@
 import os
-
+from tqdm import tqdm
 def rename_audio_files(audio_path, group_name):
     for i in range(-5, 6):
         dir_name = f"{audio_path}/{group_name}#{i}"
@@ -16,9 +16,17 @@ def rename_list_of_groups(audio_path, group_list):
     for group in group_list:
         rename_audio_files(audio_path=audio_path, group_name=group)
         
-        
+def rename_tsv(src_dir, dst_dir):
+    os.makedirs(dst_dir, exist_ok=True)
+    for file in tqdm(os.listdir(src_dir)):
+        src_path = os.path.join(src_dir, file)
+        dst_path =  os.path.join(dst_dir, file.replace(".tsv", "_mic.tsv"))
+        os.rename(src_path, dst_path)
+
 if __name__ == '__main__':
-    audio_path = '/vol/scratch/jonathany/datasets/Museopen16/'
+    src_dir = 'NoteEM_tsv/Guitar-set_only_audio_test'
+    dst_dir = 'NoteEM_tsv/Guitar-set_only_audio'
     group = "Beethoven Violin Sonatas"
-    rename_audio_files(audio_path=audio_path, group_name=group)
+    # rename_audio_files(audio_path=audio_path, group_name=group)
+    rename_tsv(src_dir=src_dir, dst_dir=dst_dir)
     
